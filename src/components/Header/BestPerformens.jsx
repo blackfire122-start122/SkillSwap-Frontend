5968cbfb57abcd90fa2bfcb758ba7e62c0269448
igor <gsambir@gmail.com>
2024-05-14 17:54:05 +0300

    header performers

--- src/components/Header/BestPerformens.jsx
+++ src/components/Header/BestPerformens.jsx
@@ -2,20 +2,31 @@ import userImage from "./../../images/user.png"
 import BestPerformer from "./BestPerformerItem";
 import style from "./../../styles/BestPerformers.module.css"
 import client from "../../lib/client";
+import {useEffect, useState} from "react";
 
 function BestPerformers() {
-    client.get("api/v1/bestPerformers")
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+    const [bestPerformers, setBestPerformers] = useState([])
 
+    function getBestPerformers() {
+        client.get("api/v1/bestPerformers")
+            .then(function (response) {
+                setBestPerformers(response.data)
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    useEffect(()=>{
+        getBestPerformers()
+    }, [])
 
     return (
         <div className={style.bestPerformers}>
-            <BestPerformer/>
+            {bestPerformers.map((bestPerformer)=>(
+                <BestPerformer bestPerformer={bestPerformer}/>
+            ))}
         </div>
     )
 }
--- src/components/Header/BestPerformerItem.js
+++ src/components/Header/BestPerformerItem.js
@@ -1,10 +1,26 @@
 import userImage from "./../../images/user.png"
 
-function BestPerformer() {
+function BestPerformer({bestPerformer}) {
+    const renderStars = () => {
+        const stars = []
+        const maxRating = 100
+        const starCount = Math.ceil((bestPerformer.rating / maxRating) * 5)
+
+        for (let i = 0; i < 5; i++) {
+            if (i < starCount) {
+                stars.push(<span key={i}>&#9733;</span>)
+            } else {
+                stars.push(<span key={i}>&#9734;</span>)
+            }
+        }
+
+        return stars
+    }
+
     return (
         <div>
-            <img src={userImage} alt="user"/>
-            <p>Rating</p>
+            <img src={userImage} alt={bestPerformer.name}/>
+            <div>{renderStars()}</div>
         </div>
     )
 }
